7a3dea5bdb72cc9fb9f1bf57a83666000c7c6e5e
Matin Habibi <itsmatinhimself@gmail.com>
2021-01-14 15:13:18 +0330

    Init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Diary-PowerShell
 A simple tool for managing diaries.
+
+Just copy and paste the diary.ps1 content into your powershell profile.(Use command `echo $PROFILE` in powershell to find out your profile path)
+
+This script opens diaries using `vscode`, You can change that just by changing `DiaryEditor` Value as your deoired editor. e.g `vim` or `code`.
---
+++ diary.ps1
@@ -0,0 +1,58 @@
+Set-Variable -Name "DiariesDirectory" -Value "~\Documents\Notes\Diaries"
+Set-Variable -Name "DiaryEditor" -Value "code"
+New-Item -ItemType Directory -Force -Path $DiariesDirectory | Out-Null
+function Diary {
+    function MakeNote {
+        param (
+            $Path
+        )
+        if (!(Test-Path $Path)) {
+            New-Item $Path | Out-Null
+        }
+        DiaryEditor $Path
+    }
+
+    function Add-Diary {
+        param (
+            $Year,
+            $Month,
+            $Name
+        )
+
+        $YPath = Join-Path -Path $DiariesDirectory -ChildPath $Year    
+        New-Item -ItemType Directory -Force -Path $YPath | Out-Null
+        
+        $MPath = Join-Path -Path $YPath -ChildPath $Month
+        New-Item -ItemType Directory -Force -Path $MPath | Out-Null
+        
+        $notePath = Join-Path -Path $MPath -ChildPath $Name
+        MakeNote -Path $($notePath)
+    }
+
+    
+
+    if ($args[0] -eq "today" -or $args[0] -eq "t") {
+        $DateTime = $([DateTime]::Today)
+    }
+    elseif ($args[0] -eq "tomorrow" -or $args[0] -eq "to") {
+        $DateTime = $([DateTime]::Today.AddDays(+1))
+    } elseif ($args[0] -eq "yesterday" -or $args[0] -eq "y") {
+        $DateTime = $([DateTime]::Today.AddDays(-1))
+    } else {
+        $Message = @'
+Unsupported flag, use:
+    Diary [t or today] for adding diary for today
+    Diary [to or tomorrow] for adding diary for tomorrow
+    Diary [y or yesterday] for adding diary for yesterday
+'@
+        Write-Output $Message
+        return
+    }
+
+    $YearString = $DateTime.ToString("yyyy")
+    $MonthString = $DateTime.ToString("MM")
+    $FileName = "$($DateTime.ToString('D')).md"
+    Add-Diary -Year $YearString -Month $MonthString -Name $FileName 
+
+
+}
